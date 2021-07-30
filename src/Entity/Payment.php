081c7d6710be551f0e81dff8b724487dfb7628ac
Jan Barášek <janbarasek@gmail.com>
2021-07-30 20:27:58 +0200

    Init entity.

---
+++ src/Entity/Payment.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baraja\Shop\Payment\Entity;
+
+
+use Baraja\Doctrine\Identifier\IdentifierUnsigned;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'shop__payment')]
+class Payment
+{
+	use IdentifierUnsigned;
+
+	#[ORM\Column(type: 'string', length: 32)]
+	private string $name;
+
+	#[ORM\Column(type: 'string', length: 32, unique: true)]
+	private string $code;
+
+	#[ORM\Column(type: 'text', nullable: true)]
+	private ?string $description;
+
+	#[ORM\Column(type: 'integer')]
+	private int $price;
+
+	#[ORM\Column(type: 'string', length: 7)]
+	private ?string $color = null;
+
+
+	public function __construct(string $name, string $code, int $price)
+	{
+		$this->name = $name;
+		$this->code = $code;
+		$this->price = $price;
+	}
+
+
+	public function getName(): string
+	{
+		return $this->name;
+	}
+
+
+	public function getCode(): string
+	{
+		return $this->code;
+	}
+
+
+	public function getDescription(): ?string
+	{
+		return $this->description;
+	}
+
+
+	public function getPrice(): int
+	{
+		return $this->price;
+	}
+
+
+	public function getColor(): ?string
+	{
+		return $this->color;
+	}
+
+
+	public function setColor(?string $color): void
+	{
+		$this->color = $color;
+	}
+}
